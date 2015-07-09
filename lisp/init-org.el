3866d0d510113875791584950fcdae11417e460d
littleqz <qizhihere@gmail.com>
2015-07-09 14:40:21 +0800

    add org-mode-hook for export funcs customization

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -79,65 +79,67 @@
   (interactive)
   (insert (format-time-string "%Y-%m-%d %b %H:%M:%S")))
 
-(defun org-html-template (contents info)
-  "Return complete document string after HTML conversion.
+(defun lqz/custom-org-html-funcs ()
+  (defun org-html-template (contents info)
+    "Return complete document string after HTML conversion.
 CONTENTS is the transcoded contents string.  INFO is a plist
 holding export options."
-  (concat
-   (when (and (not (org-html-html5-p info)) (org-html-xhtml-p info))
-     (let ((decl (or (and (stringp org-html-xml-declaration)
-                  org-html-xml-declaration)
-             (cdr (assoc (plist-get info :html-extension)
-                     org-html-xml-declaration))
-             (cdr (assoc "html" org-html-xml-declaration))
+    (concat
+     (when (and (not (org-html-html5-p info)) (org-html-xhtml-p info))
+       (let ((decl (or (and (stringp org-html-xml-declaration)
+                            org-html-xml-declaration)
+                       (cdr (assoc (plist-get info :html-extension)
+                                   org-html-xml-declaration))
+                       (cdr (assoc "html" org-html-xml-declaration))
 
-             "")))
-       (when (not (or (eq nil decl) (string= "" decl)))
-     (format "%s\n"
-         (format decl
-          (or (and org-html-coding-system
-               (fboundp 'coding-system-get)
-               (coding-system-get org-html-coding-system 'mime-charset))
-              "iso-8859-1"))))))
-   (org-html-doctype info)
-   "\n"
-   (concat "<html"
-       (when (org-html-xhtml-p info)
-         (format
-          " xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"%s\" xml:lang=\"%s\""
-          (plist-get info :language) (plist-get info :language)))
-       ">\n")
-   "<head>\n"
-   (org-html--build-meta-info info)
-   (org-html--build-head info)
-   (org-html--build-mathjax-config info)
-   "</head>\n"
-   "<body>\n"
-   "<div id=\"container\" class=\"col-md-8 col-sm-10 col-md-offset-2 col-sm-offset-1\">"
-   (let ((link-up (org-trim (plist-get info :html-link-up)))
-     (link-home (org-trim (plist-get info :html-link-home))))
-     (unless (and (string= link-up "") (string= link-home ""))
-       (format org-html-home/up-format
-           (or link-up link-home)
-           (or link-home link-up))))
-   ;; Preamble.
-   (org-html--build-pre/postamble 'preamble info)
-   ;; Document contents.
-   (format "<%s id=\"%s\">\n"
-       (nth 1 (assq 'content org-html-divs))
-       (nth 2 (assq 'content org-html-divs)))
-   ;; Document title.
-   (let ((title (plist-get info :title)))
-     (format "<h1 class=\"title\">%s</h1>\n" (org-export-data (or title "") info)))
-   contents
-   (format "</%s>\n"
-       (nth 1 (assq 'content org-html-divs)))
-   ;; Postamble.
-   (org-html--build-pre/postamble 'postamble info)
-   ;; Closing document.
-   "</div>"
-   "</body>\n</html>"))
+                       "")))
+         (when (not (or (eq nil decl) (string= "" decl)))
+           (format "%s\n"
+                   (format decl
+                           (or (and org-html-coding-system
+                                    (fboundp 'coding-system-get)
+                                    (coding-system-get org-html-coding-system 'mime-charset))
+                               "iso-8859-1"))))))
+     (org-html-doctype info)
+     "\n"
+     (concat "<html"
+             (when (org-html-xhtml-p info)
+               (format
+                " xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"%s\" xml:lang=\"%s\""
+                (plist-get info :language) (plist-get info :language)))
+             ">\n")
+     "<head>\n"
+     (org-html--build-meta-info info)
+     (org-html--build-head info)
+     (org-html--build-mathjax-config info)
+     "</head>\n"
+     "<body>\n"
+     "<div id=\"container\" class=\"col-md-8 col-sm-10 col-md-offset-2 col-sm-offset-1\">"
+     (let ((link-up (org-trim (plist-get info :html-link-up)))
+           (link-home (org-trim (plist-get info :html-link-home))))
+       (unless (and (string= link-up "") (string= link-home ""))
+         (format org-html-home/up-format
+                 (or link-up link-home)
+                 (or link-home link-up))))
+     ;; Preamble.
+     (org-html--build-pre/postamble 'preamble info)
+     ;; Document contents.
+     (format "<%s id=\"%s\">\n"
+             (nth 1 (assq 'content org-html-divs))
+             (nth 2 (assq 'content org-html-divs)))
+     ;; Document title.
+     (let ((title (plist-get info :title)))
+       (format "<h1 class=\"title\">%s</h1>\n" (org-export-data (or title "") info)))
+     contents
+     (format "</%s>\n"
+             (nth 1 (assq 'content org-html-divs)))
+     ;; Postamble.
+     (org-html--build-pre/postamble 'postamble info)
+     ;; Closing document.
+     "</div>"
+     "</body>\n</html>")))
 
+(add-hook 'org-mode-hook 'lqz/custom-org-html-funcs)
 
 ;; export settings
 (setq org-publish-project-alist
