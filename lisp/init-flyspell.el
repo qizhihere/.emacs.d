7535066aab74e7408fe1bb43dd0bc5f5b7332828
littleqz <qizhihere@gmail.com>
2015-07-20 01:24:43 +0800

    only allow flyspell run in some edit modes

--- lisp/init-flyspell.el
+++ lisp/init-flyspell.el
@@ -3,11 +3,8 @@
 
 (setq-default flyspell-mode t)
 
-;; (dolist (hook '(text-mode-hook markdown-mode-hook org-mode-hook))
-;;  (add-hook hook (lambda () (flyspell-mode 1))))
-
-(dolist (hook '(change-log-mode-hook log-edit-mode-hook))
- (add-hook hook (lambda () (flyspell-mode -1))))
+(dolist (hook '(text-mode-hook markdown-mode-hook org-mode-hook))
+ (add-hook hook (lambda () (flyspell-mode 1))))
 
 
 
