0e87ec606c53173d6374c8b69ff674d60fc26f4d
littleqz <qizhihere@gmail.com>
2015-08-18 16:17:06 +0800

    adjust workgroups2 configs order

--- lisp/init-workgroups2.el
+++ lisp/init-workgroups2.el
@@ -1,18 +1,17 @@
 (my/install 'workgroups2)
-(after-init
-  (after-load 'workgroups2
-	(diminish 'workgroups-mode)
-	(setq wg-prefix-key (kbd "C-c z")
-		  wg-session-file (my/init-dir "session/workgroups")
-		  wg-mess-with-buffer-list t))
 
-  (global-set-key (kbd "<pause>")   'wg-reload-session)
-  (global-set-key (kbd "C-<pause>") 'wg-save-session)
-  (global-set-key (kbd "M-\\")      'wg-switch-to-workgroup)
-  (global-set-key (kbd "M-[")       'wg-switch-to-previous-workgroup)
+(after-load 'workgroups
+  (diminish 'workgroups-mode)
+  (setq wg-prefix-key (kbd "C-c z")
+		wg-session-file (my/init-dir "session/workgroups")
+		wg-mess-with-buffer-list t))
 
-  (workgroups-mode 1))
+(global-set-key (kbd "<pause>")   'wg-reload-session)
+(global-set-key (kbd "C-<pause>") 'wg-save-session)
+(global-set-key (kbd "M-\\")      'wg-switch-to-workgroup)
+(global-set-key (kbd "M-[")       'wg-switch-to-previous-workgroup)
 
+(after-init (workgroups-mode 1))
 
 
 (provide 'init-workgroups2)
