98643316339174e7d56bf13bb0a120d247d70280
littleqz <qizhihere@gmail.com>
2015-08-18 23:48:23 +0800

    change `Lisp Interaction Mode` name

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -3,6 +3,8 @@
 (remove-hook 'emacs-lisp-mode-hook 'flycheck-mode)
 (after-load 'eldoc
   (diminish 'eldoc-mode))
+(add-hook 'lisp-interaction-mode-hook
+		  (lambda () (setq mode-name "Lisp")))
 
 
 
