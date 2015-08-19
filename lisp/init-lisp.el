cc815b97b675a204af7273c21c7ab529c59a7750
littleqz <qizhihere@gmail.com>
2015-08-20 01:46:52 +0800

    add eldoc-eval and ipretty

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,8 +1,20 @@
-(my/install 'eldoc)
-(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(my/install '(eldoc eldoc-eval ipretty))
+
+;; eval-expression result pretty print
+(ipretty-mode 1)
+(defadvice pp-display-expression (after my/make-read-only (expression out-buffer-name) activate)
+  "enable `view-mode' in the output buffer - if any - so it can be closed with `\"q\"."
+  (when (get-buffer out-buffer-name)
+	(with-current-buffer out-buffer-name
+	  (view-mode 1)
+	  (and (fboundp 'evil-change-state)
+		   (evil-change-state 'emacs)))))
+
 (remove-hook 'emacs-lisp-mode-hook 'flycheck-mode)
-(after-load 'eldoc
-  (diminish 'eldoc-mode))
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'eldoc-mode-hook 'eldoc-in-minibuffer-mode)
+
+(after-load 'eldoc (diminish 'eldoc-mode))
 (add-hook 'lisp-interaction-mode-hook
 		  (lambda () (setq mode-name "Lisp")))
 
