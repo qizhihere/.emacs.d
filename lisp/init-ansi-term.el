1cc22bd815038d32df90562031615e92b0cda26e
littleqz <qizhihere@gmail.com>
2015-08-20 18:10:09 +0800

    add multi-term switch keys

--- lisp/init-ansi-term.el
+++ lisp/init-ansi-term.el
@@ -6,11 +6,15 @@
   (let ((term (if (package-installed-p 'multi-term) 'multi-term 'ansi-term)))
     (global-set-key [f12] term)))
 
+(after-load 'multi-term
+  (global-set-key (kbd "C-<f12>") 'multi-term-next)
+  (global-set-key (kbd "M-<f12>") 'multi-term-prev))
+
 (add-hook 'term-mode-hook
           (lambda ()
             (setq show-trailing-whitespace nil)
             (electric-pair-mode 1)
-            (toggle-truncate-lines)
+            (setq truncate-lines nil)
             (setq term-prompt-regexp "^.*\\(❯❯❯\\|»\\|➤➤\\) ")
             (make-local-variable 'mouse-yank-at-point)
             (setq mouse-yank-at-point t)
