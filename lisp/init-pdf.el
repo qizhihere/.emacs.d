0f2f2c13a77c70e4b0106f07953ba1e8fd8b19a5
littleqz <qizhihere@gmail.com>
2015-09-11 15:17:25 +0800

    use touchpad to horizontally scroll in pdf-view-mode

--- lisp/init-pdf.el
+++ lisp/init-pdf.el
@@ -1,10 +1,16 @@
 ;; pdf tools
 (my/install 'pdf-tools)
 (pdf-tools-install)
-(define-key pdf-view-mode-map [down-mouse-1] 'pdf-view-mouse-set-region)
-(define-key pdf-view-mode-map [C-down-mouse-1] 'pdf-view-mouse-extend-region)
-(define-key pdf-view-mode-map [M-down-mouse-1] 'pdf-view-mouse-set-region-rectangle)
 
+(setq my/pdf-view-mode-keypairs
+      '(([down-mouse-1] pdf-view-mouse-set-region)
+        ([C-down-mouse-1] pdf-view-mouse-extend-region)
+        ([M-down-mouse-1] pdf-view-mouse-set-region-rectangle)
+        ([triple-mouse-6] image-backward-hscroll)
+        ([triple-mouse-7] image-forward-hscroll)))
+
+(dolist (pair my/pdf-view-mode-keypairs)
+  (define-key pdf-view-mode-map (car pair) (cadr pair)))
 
 
 (provide 'init-pdf)
