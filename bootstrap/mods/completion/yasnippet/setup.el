7c0d3a23ca3c864c35c1950a5d18c64e9cd66325
littleqz <qizhihere@gmail.com>
2016-05-12 20:02:13 +0800

    Use <tab> to indent or complete.

--- bootstrap/mods/completion/yasnippet/setup.el
+++ bootstrap/mods/completion/yasnippet/setup.el
@@ -12,13 +12,36 @@
     (add-hook 'snippet-mode (lambda () (setq-local m|whitespace-cleanup-style 'trailing)))
 
     (loaded company
-      (defun m|company-yasnippet ()
+      (defun m|indent-and-complete ()
         "Indent with `indent-for-tab-command', or show yasnippet candidates."
         (interactive)
         (let ((pos (point)))
           (indent-for-tab-command)
           (when (eq pos (point))
-            (call-interactively #'company-yasnippet))))
-      (bind-key "<tab>" #'m|company-yasnippet yas-minor-mode-map))
+            (silently (company-complete-common))
+            (unless company-candidates
+              (m|company-yasnippet)))))
+
+      (defun m|company-yasnippet ()
+        (interactive)
+        (company-abort)
+        (let ((prefix (company-yasnippet 'prefix)))
+          (when prefix
+            (let ((candidates (company-yasnippet 'candidates prefix)))
+              (if (and
+                   (= (length candidates) 1)
+                   (string= prefix (car candidates)))
+                  (yas-expand)
+                (company-begin-backend 'company-yasnippet))))))
+
+      (bind-keys :map yas-minor-mode-map
+        ("<tab>" . m|indent-and-complete)
+        ("M-j" . m|company-yasnippet))
+
+      ;; abort company before yasnippet expansion
+      (defun m|yas-abort-company (&rest args)
+        (when (fboundp 'company-abort)
+          (company-abort)))
+      (advice-add 'yas-expand :before #'m|yas-abort-company))
 
     (silently (yas-reload-all))))
