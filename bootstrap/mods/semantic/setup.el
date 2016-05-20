db32366e08267f86fa69d540ec64d170d428d576
littleqz <qizhihere@gmail.com>
2016-05-20 11:18:11 +0800

    Set semanticdb directory.

--- bootstrap/mods/semantic/setup.el
+++ bootstrap/mods/semantic/setup.el
@@ -2,4 +2,6 @@
   (use-package semantic
     :defer t
     :init
-    (add-hook 'prog-mode-hook #'semantic-mode)))
+    (add-hook 'prog-mode-hook #'semantic-mode)
+    :config
+    (setq semanticdb-default-save-directory (m|cache-dir "semanticdb"))))
