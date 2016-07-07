646e5829ff8b69df6b91d028e5d918b09a75343b
littleqz <qizhihere@gmail.com>
2016-07-07 12:21:16 +0800

    Add `nginx-mode` file pattern.

--- bootstrap/mods/lang/lang+/setup.el
+++ bootstrap/mods/lang/lang+/setup.el
@@ -4,3 +4,8 @@
                        lua-mode
                        dockerfile-mode
                        nginx-mode))
+
+(defun lang+/init-nginx()
+  (use-package nginx-mode
+    :defer t
+    :mode "/nginx/.+"))
