79cce8edd04feb1c8a3cee29bc6df55ac65be27f
littleqz <qizhihere@gmail.com>
2016-08-15 15:49:28 +0800

    Fix swiper regex issue.

--- bootstrap/mods/ivy+/setup.el
+++ bootstrap/mods/ivy+/setup.el
@@ -27,6 +27,7 @@
 
     (setq ivy-re-builders-alist
           '((counsel-unicode-char . ivy--regex-plus)
+            (swiper . ivy--regex-plus)
             (t . ivy--regex-fuzzy)))))
 
 (defun ivy+/post-init-magit ()
