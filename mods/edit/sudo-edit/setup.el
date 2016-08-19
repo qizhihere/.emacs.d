fc6557a7635ae01ea84e36adc39b8589785f160c
littleqz <qizhihere@gmail.com>
2016-08-19 15:07:38 +0800

    Use <f1> to toggle sudo edit.

--- mods/edit/sudo-edit/setup.el
+++ mods/edit/sudo-edit/setup.el
@@ -2,16 +2,16 @@
                            dired-toggle-sudo))
 
 (defun sudo-edit/init ()
-  (use-package sudo-edit
-    :leader ("tfs" sudo-edit-current-file
-             "ofs" sudo-edit)))
+  (use-package sudo-edit :defer t))
 
 (defun sudo-edit/init-dired ()
   (use-package dired-toggle-sudo
-    :leader ("ts" dired-toggle-sudo))
+    :leader ("ts" dired-toggle-sudo)
+    :bind (("<f1>" . dired-toggle-sudo)))
 
   (loaded dired
     (m|unbind-key "t" dired-mode-map :if #'commandp)
     (bind-keys :map dired-mode-map
       ("tt" . dired-toggle-marks)
-      ("ts" . dired-toggle-sudo))))
+      ("ts" . dired-toggle-sudo)
+      ("<f1>" . dired-toggle-sudo))))
