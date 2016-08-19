e3c3c7d5325f67c7cdbe7ac71c7c13b1a6a6c372
littleqz <qizhihere@gmail.com>
2016-08-19 10:35:05 +0800

    Customize flycheck fringes.

--- bootstrap/mods/syntax/setup.el
+++ bootstrap/mods/syntax/setup.el
@@ -16,7 +16,43 @@
              "er" flycheck-list-errors)
     :config
     (setq flycheck-idle-change-delay 2
-          flycheck-temp-prefix ".flycheck")))
+          flycheck-temp-prefix ".flycheck")
+
+    ;; custom fringes
+    (when (fboundp 'define-fringe-bitmap)
+      (define-fringe-bitmap 'my-flycheck-fringe-indicator
+        (vector #b00000000
+                #b00000000
+                #b00000000
+                #b00000000
+                #b00000000
+                #b00000000
+                #b00000000
+                #b00011100
+                #b00111110
+                #b00111110
+                #b00111110
+                #b00011100
+                #b00000000
+                #b00000000
+                #b00000000
+                #b00000000
+                #b00000000)))
+
+    (flycheck-define-error-level 'error
+      :overlay-category 'flycheck-error-overlay
+      :fringe-bitmap 'my-flycheck-fringe-indicator
+      :fringe-face 'flycheck-fringe-error)
+
+    (flycheck-define-error-level 'warning
+      :overlay-category 'flycheck-warning-overlay
+      :fringe-bitmap 'my-flycheck-fringe-indicator
+      :fringe-face 'flycheck-fringe-warning)
+
+    (flycheck-define-error-level 'info
+      :overlay-category 'flycheck-info-overlay
+      :fringe-bitmap 'my-flycheck-fringe-indicator
+      :fringe-face 'flycheck-fringe-info)))
 
 (defun syntax/init-flyspell ()
   (use-package flyspell
