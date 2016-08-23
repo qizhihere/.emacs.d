36b5c5f78303f363e873c63236dcd1dadab92745
littleqz <qizhihere@gmail.com>
2016-08-23 11:11:51 +0800

    Fix comment indentation.

--- core/core-vars.el
+++ core/core-vars.el
@@ -25,14 +25,14 @@ session, auto saved files, and recently opened files will be put here.")
 
 (defvar m|whitespace-cleanup-style 'all
   "Tell `m|clean-whitespace' how to clean whitespace. `all' means
-  clean all problematic whitespaces including empty lines,
-  `trailing' means only clean trailing whitespaces.")
+clean all problematic whitespaces including empty lines,
+`trailing' means only clean trailing whitespaces.")
 
 (defvar elpa-mirror-directory "~/.emacs.d/elpa-mirror")
 
 (defvar m|use-elpa-mirror nil
-  "When enabled, use elpa mirror in `elpa-mirror-directory'. And all other archive
-repositories will be disabled.")
+  "Use elpa mirror in `elpa-mirror-directory'. If enabled,
+all other archive repositories will be disabled.")
 
 (defvar m|elpa-mirror-auto-mirroring t
   "Whether automatically run `mirror-elpa' at time points
