c2a4ed71cb862247a54cb872bd5401bfe66a9b5a
littleqz <qizhihere@gmail.com>
2016-08-23 13:54:45 +0800

    Enable in-buffer diff-hl mode.

--- mods/cvs/setup.el
+++ mods/cvs/setup.el
@@ -7,6 +7,7 @@
   (use-package diff-hl
     :defer t
     :init
-    (m|add-startup-hook '(global-diff-hl-mode diff-hl-flydiff-mode))
+    (m|add-startup-hook #'global-diff-hl-mode)
+    (when window-system (m|add-startup-hook #'diff-hl-flydiff-mode))
     (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
     (add-hook 'dired-mode-hook 'diff-hl-dired-mode)))
